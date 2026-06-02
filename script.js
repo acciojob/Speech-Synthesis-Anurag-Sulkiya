8f95a8047c3dd710fc407bffb4bd60a1eee81058
Anurag Sulkiya <163811905+Anurag-Sulkiya@users.noreply.github.com>
2026-06-02 10:05:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-// Your script here.
+msg.text = document.querySelector('[name="voice"]');
+
+function populateVoiceDropdown(){
+    voices = window.speechSynthesis.getVoices();
+	const voiceOptions = voices.map(function(voice) {
+	return `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`
+	})
+	voicesDropdown.innerHTML = voiceOptions.join('');
+	
+}
+
+function speak() {
+	window.speechSynthesis.cancel();
+	const chosenVoiceName = voicesDropdown.value;
+	msg.voice = voices.find(function (voice) {
+		return voice.name = chosenVoiceName;
+	})
+	 window.speechSynthesis.speak(msg);
+}
+
+function stopSpeaking(){
+   window.speechSynthesis.cancel();
+}
+
+function applyOptionChange(event) {
+	msg[event.target.name] = event.target.value;
+}
+
+window.speechSynthesis.addEventListener('voiceschanged', populateVoiceDropdown);
+
+voicesDropdown.addEventListener('change', speak);
+
+options.forEach(function(option) {
+    option.addEventListener('change', applyOptionChange);
+  });
+
+speakButton.addEventListener('click', speak);
+ stopButton.addEventListener('click', stopSpeaking);
+
